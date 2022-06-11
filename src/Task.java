85d21b176a3434dfe987e6db5e0e85ab066e009b
Gali <4444ga4444@gmail.com>
2022-06-11 19:38:28 +0300

    Part 2 classes

--- src/Task.java
+++ src/Task.java
@@ -1,9 +1,24 @@
+import java.util.Date;
+
 /**
  * Task.java represent a task of student
  * @version 11/06/22
  * @author Yuval komar, Gali arba
  */
-public class Task {
+public class Task extends Date implements Cloneable {
     private String Description;
+    private Date dueDate;
+
+    public Task(String description, Date dueDate){ // Constructor
+        this.Description = description;
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public Task clone() {
+        return null;
+    }
+
+
 
 }
---
+++ src/ToDoList.java
@@ -0,0 +1,16 @@
+/**
+ * ToDoList.java represent list of tasks
+ * @version 11/06/22
+ * @author Yuval komar, Gali arba
+ */
+
+public class ToDoList implements Cloneable {
+    Task[] tasks;
+
+    // Constructor
+
+    @Override
+    public ToDoList clone() { //override clone action
+        return null;
+    }
+}
