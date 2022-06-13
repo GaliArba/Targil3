eb329783c3cf9750e35d9c7ed741fc3378edc725
Gali <4444ga4444@gmail.com>
2022-06-14 01:02:46 +0300

    fucking 1 balayila

--- src/Task.java
+++ src/Task.java
@@ -13,11 +13,20 @@ public class Task extends Date implements Cloneable {
     private String Description;
     private Date dueDate;
 
+    /**
+     * constructor of the class
+     * @param description string that describe the task
+     * @param dueDate Date that describe until when you can apply the task
+     */
     public  Task(String description, Date dueDate){ // Constructor
         this.Description = new String(description);
         this.dueDate = dueDate;
     }
 
+    /**
+     * the method prints the task due to format
+     * @return task
+     */
     @Override
     public String toString(){
         return "(" + Description + ", " + dueDate + ")";
@@ -28,6 +37,23 @@ public class Task extends Date implements Cloneable {
         return null;
     }
 
+    /**
+     * this method checks if 2 tasks are equals
+     * @param task that we'd like to compare
+     * @return boolean value
+     */
+    @Override
+    public boolean equals(Task task){
+        //TODO write that
+    }
 
+    /**
+     * return a hashCode value for the task object
+     * @return hashCode
+     */
+    @Override
+    public int hashCode(){
+        //TODO write that method
+    }
 
 }
